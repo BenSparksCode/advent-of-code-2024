5205f56a6236354943a4db6bfadcb3a444ab3c97
Ben Sparks <52714090+BenSparksCode@users.noreply.github.com>
2024-12-08 08:41:37 +0200

    chore: remove unit tests from all days

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -82,20 +82,3 @@ pub fn part_two(input: &str) -> Option<u32> {
 
     Some(count)
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_part_one() {
-        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, Some(2));
-    }
-
-    #[test]
-    fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, Some(4));
-    }
-}
--- src/bin/05.rs
+++ src/bin/05.rs
@@ -131,20 +131,3 @@ pub fn part_two(input: &str) -> Option<u32> {
 
     Some(sum_mids as u32)
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_part_one() {
-        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
-    }
-
-    #[test]
-    fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
-    }
-}
