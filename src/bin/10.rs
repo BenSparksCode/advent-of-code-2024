22565c5e2e5b103b1fd5baaac67008b5b6971798
Ben Sparks <52714090+BenSparksCode@users.noreply.github.com>
2024-12-10 09:47:08 +0200

    feat: solve Day 10 Part 2

--- src/bin/10.rs
+++ src/bin/10.rs
@@ -9,7 +9,7 @@ const DIRS: [(i32, i32); 4] = [
     (-1, 0), // LEFT
 ];
 
-pub fn dfs(
+pub fn dfs_1(
     grid: &Vec<Vec<i32>>,
     memo: &mut HashMap<(i32, i32), HashSet<(i32, i32)>>,
     p: (i32, i32),
@@ -36,7 +36,7 @@ pub fn dfs(
         if next.0 >= 0 && next.0 <= max_x && next.1 >= 0 && next.1 <= max_y {
             // Only move to next if height increases by 1
             if grid[next.1 as usize][next.0 as usize] == height + 1 {
-                set.extend(dfs(grid, memo, next, max_x, max_y));
+                set.extend(dfs_1(grid, memo, next, max_x, max_y));
             }
         }
     }
@@ -45,6 +45,45 @@ pub fn dfs(
     memo.insert(p, set.clone());
 
     set
+}
+
+pub fn dfs_2(
+    grid: &Vec<Vec<i32>>,
+    memo: &mut HashMap<(i32, i32), i32>,
+    p: (i32, i32),
+    max_x: i32,
+    max_y: i32,
+) -> i32 {
+    // If p in memo, already explored -> return value
+    if let Some(res) = memo.get(&p) {
+        return *res;
+    }
+
+    // if p is a peak (9), stop exploring, return 1
+    let height = grid[p.1 as usize][p.0 as usize];
+    if height == 9 {
+        return 1;
+    }
+
+    // Otherwise explore in all 4 directions if possible
+    let mut score = 0;
+
+    for d in DIRS {
+        let next = (p.0 + d.0, p.1 + d.1);
+
+        // Only move to next if next is in bounds
+        if next.0 >= 0 && next.0 <= max_x && next.1 >= 0 && next.1 <= max_y {
+            // Only move to next if height increases by 1
+            if grid[next.1 as usize][next.0 as usize] == height + 1 {
+                score += dfs_2(grid, memo, next, max_x, max_y);
+            }
+        }
+    }
+
+    // Once all explore directions returned, update memo for this point
+    memo.insert(p, score);
+
+    score
 }
 
 pub fn part_one(input: &str) -> Option<u32> {
@@ -66,7 +105,7 @@ pub fn part_one(input: &str) -> Option<u32> {
         for (x, n) in row.iter().enumerate() {
             // Only explore from trailheads
             if *n == 0 {
-                sum += dfs(&grid, &mut memo, (x as i32, y as i32), max_x, max_y).len();
+                sum += dfs_1(&grid, &mut memo, (x as i32, y as i32), max_x, max_y).len();
             }
         }
     }
@@ -75,5 +114,28 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let grid: Vec<Vec<i32>> = input
+        .lines()
+        .map(|line| {
+            line.chars()
+                .map(|c| c.to_digit(10).unwrap() as i32)
+                .collect()
+        })
+        .collect();
+    let mut memo: HashMap<(i32, i32), i32> = HashMap::new();
+    let max_x = (grid[0].len() - 1) as i32;
+    let max_y = (grid.len() - 1) as i32;
+
+    let mut sum = 0;
+
+    for (y, row) in grid.iter().enumerate() {
+        for (x, n) in row.iter().enumerate() {
+            // Only explore from trailheads
+            if *n == 0 {
+                sum += dfs_2(&grid, &mut memo, (x as i32, y as i32), max_x, max_y);
+            }
+        }
+    }
+
+    Some(sum as u32)
 }
